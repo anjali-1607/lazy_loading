6905749ec374ab72f90a9746740164842e200efb
anjali-1607 <anjalirajpoot1609@gmail.com>
2024-06-20 20:15:50 +0530

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./About.jsx";
+// const About = lazy(() => import("./About"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                {/* <Suspense fallback={<p>Loading</p>}> */}
+                <Route path="/" element={<App />} />
+                <Route path="/about" element={<About />} />
+                {/* </Suspense> */}
+            </Routes>
+        </BrowserRouter>
+        {/* <App /> */}
+    </React.StrictMode>
+);
